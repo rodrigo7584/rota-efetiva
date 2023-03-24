97e2726384c9e6f01c10e2e3a5e3ebbc16106548
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2023-03-24 17:29:50 -0300

    Entregar resultados é fator chave para o sucesso profissional, aqui na mentoria você irá aprender a se diferenciar dos concorrentes pelo aumento considerável das suas entregas.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+const accordion = document.querySelectorAll('.accordion')
+
+accordion.forEach(item => {
+  const question = item.querySelector('.question')
+  const answer = item.querySelector('.answer')
+
+  const answerHeight = answer.scrollHeight
+  answer.style.maxHeight = 0
+
+  question.addEventListener('click', () => {
+    // answer.classList.toggle('open')
+    if (item.classList.contains('open')) {
+      answer.style.maxHeight = 0
+      item.classList.remove('open')
+    } else {
+      answer.style.maxHeight = answerHeight + 'px'
+      item.classList.add('open')
+    }
+  })
+})
+
 const caroucelPrints = document.querySelector(
   '.caroucel-prints .caroucel-content'
 )
